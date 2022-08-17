7003cb0a1d9ce7a83db0be4b756014fa9f539a49
GavrilovIY <GavrilovIY@IVANB3V>
2022-08-17 13:51:34 +0300

    + EFCore

---
+++ WebApiHW_8.08.22/DBContext/UserDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiHW_8._08._22.Interfaces;
+
+namespace WebApiHW_8._08._22.DBContext
+{
+    internal sealed class UserDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=;Database=MyNewDB;Username=postgres;Password=123456; ");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().Ignore(x => x.Comment);
+        }
+    }
+    internal sealed class ClientDbContext : DbContext
+    {
+
+    }
+    class User : IPerson
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public string Comment { get; set; }
+    }
+}
