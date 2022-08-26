c99a735434e332857b10a98b5d91c055890abc61
GavrilovIY <GavrilovIY@IVANB3V>
2022-08-26 14:05:22 +0300

    Validation step 1

---
+++ WebApiHW_8.08.22/Interfaces/IOperationFailure.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApiHW_8._08._22.Interfaces;
+public interface IOperationFailure
+{
+    string PropertyName { get; }
+    string Description { get; }
+    string Code { get; }
+}
+public interface IValidationService<TEntity> where TEntity : class
+{
+    IReadOnlyList<IOperationFailure> ValidateEntity(TEntity item);
+}
---
+++ WebApiHW_8.08.22/Services/Validation/UserValidationService.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+using WebApiHW_8._08._22.Repository.Models;
+
+namespace WebApiHW_8._08._22.Services.Validation;
+public class UserValidationService:AbstractValidator<User>
+{
+    public UserValidationService()
+    {
+        RuleFor(x => x.Name).NotEmpty().WithMessage("Name Must be NOT empty!");
+        RuleFor(x => x.Password).NotEmpty().WithMessage("Password Must be NOT empty!");
+    }
+}
